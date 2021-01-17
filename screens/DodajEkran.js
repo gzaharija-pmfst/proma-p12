a551eb0151a6eddb3b1cad84723c5246b0964f85
gzaharija <gzaharija@pmfst.hr>
2021-01-17 12:35:41 +0100

    Forma unos

--- screens/DodajEkran.js
+++ screens/DodajEkran.js
@@ -1,12 +1,32 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { HeaderTitle } from "react-navigation-stack";
+import React, {useState} from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  Button,
+} from "react-native";
+import Boje from "../constants/Boje";
 
 const DodajEkran = (props) => {
+ const [naslovUnos, postaviNaslov] = useState('')
+ const noviNaslov = novi => {
+   // Trenutno nema validacije
+   postaviNaslov(novi )
+ }
   return (
-    <View>
-      <Text>Ekran za dodavanje nove lokacije</Text>
-    </View>
+    <ScrollView>
+      <View style={stil.forma}>
+        <Text style={stil.oznaka}>Naziv</Text>
+        <TextInput style={stil.unosTeksta} value={naslovUnos} onChangeText={noviNaslov} />
+        <Button
+          title="Spremi lokaciju"
+          color={Boje.glavna}
+          onPress={() => {}}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
@@ -22,6 +42,21 @@ const stil = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  forma: {
+    margin: 30,
+  },
+  unosTeksta: {
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2
+
+  },
+  oznaka: {
+    fontSize: 18,
+    marginBottom: 15,
   },
 });
 
